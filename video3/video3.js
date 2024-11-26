485729628c055e09898341dfc634add787bcf7eb
hoainhondinh <hoainhondinh@gmail.com>
2024-11-26 11:49:56 +0700

    code vd03

---
+++ video3/video3.js
@@ -0,0 +1,12 @@
+let a = 5;
+let b = 5.9;
+
+console.log("Hello world JS!", a);
+
+let x = 5, y = 6, z = 7;
+console.log("Testing string" , 'x =', x ,'y =', y ,'z =', z);
+// const x = 5; //Hằng số sinh ra để giá trị không đổi
+
+// x = 6;
+
+// console.log("x: ", x);//Sẽ in ra lỗi
