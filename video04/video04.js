847e10c9dabb0453ad6038a69e2d57e5887b2fa1
hoainhondinh <hoainhondinh@gmail.com>
2024-11-27 12:16:31 +0700

    video4

---
+++ video04/video04.js
@@ -0,0 +1,11 @@
+let a = 'Toi la string - chuoi';
+let text = "We are the so-called 'Vikings' from the north.";
+let b = `I'm a bad boy`;
+console.log(a)
+console.log(text)   
+console.log(b)   
+
+
+// console.log(4 + 5 + 'a') = 9a
+
+// console.log('a' + 4 + 5) = a45
