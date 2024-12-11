bb0c6fb8485c7148ce8a35bff9cdd56788ca1c22
hoainhondinh <hoainhondinh@gmail.com>
2024-12-11 17:20:24 +0700

    update video10

---
+++ video10/video10.js
@@ -0,0 +1,13 @@
+console.log('Hello world!');
+
+let a = 5;
+let b = '5';
+
+console.log('compare a == b :', 'a =', a , 'b = ', b, 'result :', a == b );
+console.log('compare a === b :', 'a =', a , 'b = ', b, 'result :', a === b );
+
+// === type, value 
+// == value
+//convention(rule)
+
+//empty, null, undefined(loi)
