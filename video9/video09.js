b2f8c02686439e30c539b1a95cb0f37b50dde77f
hoainhondinh <hoainhondinh@gmail.com>
2024-12-11 16:53:33 +0700

    update video9

---
+++ video9/video09.js
@@ -0,0 +1,21 @@
+console.log("hello world")
+
+let a = {
+    name: "John",
+    age: 30
+}
+console.log("check data object", a)
+console.log("my name \'s", a.name)
+// object quan ly thong qua key 
+// array bản chất là obj nhưng kiểu quản lý dữ liệu khác, array co nhieu method ho tro
+//arr = [item1, item2, item3]
+let b = ['MU', 'Chel','Liver'];
+console.log('type of b:', typeof b, b)
+console.log('first item:', b[0], 'second item:', b[1], 'third item:', b[2])
+
+let family = [
+    {name: "John", age: 30},
+    {name: "Alice", age: 28},
+    {name: "Bob", age: 32}
+]
+console.log('my family:', family)
