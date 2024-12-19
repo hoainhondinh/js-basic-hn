8af95148f5ccca4f793b5b50ff6e1ef3bc1e2b71
hoainhondinh <hoainhondinh@gmail.com>
2024-12-19 12:32:44 +0700

    while loops do while loops

---
+++ video12/video12.js
@@ -0,0 +1,19 @@
+console.log("Hello world!");
+//2 While loops/ do while loops
+let arrTop4 = ['MC', 'Liv', 'Mu', 'Chel'];
+
+// for (let i = 0; i < arrTop4.length; i++){
+//     console.log('Top:', i + 1 , arrTop4[i]);
+// }
+
+let i = 0
+while (i < arrTop4.length){
+    console.log('Top:', i + 1, arrTop4[i]);
+    i++;
+}
+
+// do {
+//     console.log('Top:', i + 1, arrTop4[i]);
+//     i++;
+// } while (i < 0);
+// sai nhưng vẫn chạy 1 lần 
