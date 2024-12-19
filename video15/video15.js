67338f830298237599f5b4fc1e2a01fa32925341
hoainhondinh <hoainhondinh@gmail.com>
2024-12-19 13:46:18 +0700

    variables, block scope

---
+++ video15/video15.js
@@ -0,0 +1,10 @@
+//variables, block scope variables Biến & phạm vi sử dụng biến
+
+console.log('Hello world!15');
+
+//global
+//block
+
+for (let i = 0; i < 10; i++){
+    console.log('Check value i:' ,i);
+} 
