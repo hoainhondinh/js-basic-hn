a2823abe8412333924ed34b73143d2e2fb6c0186
hoainhondinh <hoainhondinh@gmail.com>
2024-12-20 11:42:58 +0700

    arrow function

---
+++ video18/video18.js
@@ -0,0 +1,11 @@
+//arrow function
+console.log('arrow function');
+
+// function sum(a, b) {
+//     return a + b;
+// }
+
+let sum = (a,b) => {
+    return a + b;
+}
+console.log('sum: ', sum(10, 20));
